61f0d25d01e3e02a82a9fb4a56291f4e2f428b37
mario <daw@grupapunts.es>
2024-12-02 10:19:49 +0100

    crud hecho

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,8 +1,36 @@
 const Post = require("../models/Post")
 
 const PostController = {
-
-    
+    async create(req,res){
+        try {
+            const newPost = await Post.create(req.body)
+        res.status(201).send({message:"New post successfully created",newPost})
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({message:"There was a problem",error})
+        } 
+    },
+    async delete(req, res) {
+        try {
+            const post = await Post.findByIdAndDelete(req.params._id)
+            res.send({ message: 'Post deleted', post })
+        } catch (error) {
+            console.error(error)
+            res.status(500).send({ message: 'there was a problem trying to remove the post'})
+        }
+    },
+    async update(req, res) {
+        try {
+          const post = await Post.findByIdAndUpdate(
+            req.params._id, //id del post que quiero actualizar
+            req.body,// el objeto con los datos a actualizar 
+            { new: true }// para que el post de la respuesta sea el actualizado
+        )
+          res.send({ message: "post successfully updated", post });
+        } catch (error) {
+          console.error(error);
+        }
+      },
 }
 
 module.exports = PostController
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,9 @@ const express = require("express")
 const PostController = require("../controllers/PostController")
 const router = express.Router()
 
+router.post("/create",PostController.create)
+router.delete("/delete",PostController.create)
+router.put("/update",PostController.create)
 
 
 module.exports = router
