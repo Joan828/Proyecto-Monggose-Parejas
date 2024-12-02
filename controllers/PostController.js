fc829653dd02d4f7270402f459189cbbe312e498
unknown <joan_mart_her@hotmail.com>
2024-12-02 13:15:18 +0100

    Añadimos el getPostById

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,6 +2,14 @@ const Post = require("../models/Post")
 
 const PostController = {
 
+    async getPostById(req, res) {
+        try {
+            const post = await Post.findById(req.params._id)
+            res.send(post)
+        } catch (error) {
+            console.error(error);
+        }
+    }
     
 }
 
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,6 @@ const express = require("express")
 const PostController = require("../controllers/PostController")
 const router = express.Router()
 
-
+router.get("/getPostById/:_id", PostController.getPostById)
 
 module.exports = router
