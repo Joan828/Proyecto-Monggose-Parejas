f2117689e5968f36865a22d3240bbdad15201edd
mario <daw@grupapunts.es>
2024-12-04 12:15:53 +0100

    create comment a media

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -3,6 +3,17 @@ const Comment = require("../models/Comment")
 const CommentController = {
 
     
+    async create(req, res) {
+        try {
+          const comment = await Comment.create({
+            ...req.body,
+            userId: req.user._id,
+          });
+          res.status(201).send(comment);
+        } catch (error) {
+          console.error(error);
+        }
+      },
 }
 
 module.exports = CommentController
--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,8 @@
 const express = require("express")
+const CommentController = require("../controllers/CommentController")
+const { authentication, isAuthor } = require("../middleware/authentication")
 const router = express.Router()
 
-
+router.post("/create", authentication, CommentController.create)
 
 module.exports = router
