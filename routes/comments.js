8cde884172d9a45975e7b9837665ae54d9f5d838
unknown <joan_mart_her@hotmail.com>
2024-12-04 12:46:58 +0100

    Error Comment fixed

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const CommentController = require("../controllers/CommentController")
+const { authentication } = require("../middleware/authentication")
 const router = express.Router()
 
-router.post("/create", CommentController.create)
+router.post("/create", authentication, CommentController.create)
 router.get("/getAll", CommentController.getAll)
 router.put("/update", CommentController.update)
 router.delete("/delete", CommentController.delete)
